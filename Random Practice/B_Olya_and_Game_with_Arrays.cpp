a6ca68146f34bac56b2276f389a9b5cb3d827353
0PointOne <zishanabder@gmail.com>
2024-03-02 23:37:32 +0600

    solve 1000 rated 2 problems

---
+++ Random Practice/B_Olya_and_Game_with_Arrays.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define int long long
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define print_v(v) for(auto it : v) cout << it << " "; cout << endl;
+#define print_v_pair(v) for(auto it: v) cout << it.first << " " << it.second << endl;
+#define input_v for(auto &it : v)   cin >> it;
+
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve(){
+
+    int n;  cin >> n;
+    int sum = 0;
+    int mn_of_mn1 = INT_MAX, mn_of_mn2 = INT_MAX;
+
+    while(n--){
+        int m;  cin >> m;
+
+        int mn1 = INT_MAX, mn2 = INT_MAX;
+
+        int num;
+
+        for(int i = 0; i < m; i++){
+            cin >> num;
+            if(mn1 > num){
+
+                mn2 = mn1;
+
+                mn1 = num;
+            
+            }
+            else if(mn2 >num) mn2 = num;
+        }
+
+        sum += mn2;
+
+        mn_of_mn1 = min(mn_of_mn1, mn1);
+        mn_of_mn2 = min(mn_of_mn2, mn2);
+
+    }
+
+    debug(mn_of_mn1)
+    debug(mn_of_mn2)
+    debug(sum)
+    debug('\n')
+
+    cout << sum + mn_of_mn1 - mn_of_mn2 << nline;
+    
+}
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    fastio();
+    int t = 1;   	
+    cin >> t;
+    while(t--){     solve(); }
+    return 0;
+}
