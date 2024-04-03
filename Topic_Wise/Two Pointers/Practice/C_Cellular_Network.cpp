c344a555f345f6edae18fa28074ed51484936459
0PointOne <zishanabder@gmail.com>
2024-04-03 17:36:11 +0600

    practice 2 problem

---
+++ Topic_Wise/Two Pointers/Practice/C_Cellular_Network.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define int long long
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define print_v(v) for(auto it : v) cout << it << " "; cout << nline;
+#define print_v_pair(v) for(auto it: v) cout << it.first << " " << it.second << nline;
+#define input_v for(auto &it : v)   cin >> it;
+
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << nline;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int low(vector<int> &v, int n){
+    int s = 0, e = v.size();
+
+    int mid = s + (e - s) / 2;
+    while(s < e){
+        if(v[mid] >= n) e = mid;
+        else            s = mid + 1;
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+
+void solve(){
+
+    int n, m;   cin >> n >> m;
+    vector<int> a(n), b(m);
+    for(int i = 0; i < n; i++)  cin >> a[i];
+    for(int i = 0; i < m; i++)  cin >> b[i];
+
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        int r = low(b, a[i]);
+
+        int l = r - 1;
+
+        int mn = INT_MAX;
+        if(r < m){
+            assert(b[r] >= a[i]);
+            mn = min(mn, b[r] - a[i]);
+        }
+
+        if(l >= 0){
+            assert(b[l] <= a[i]);
+            mn = min(mn, a[i] - b[l]);
+        }
+
+        ans = max(ans, mn);
+    }
+    cout << ans << nline;
+}
+    
+
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    fastio();
+    int t = 1;   	
+    // cin >> t;
+    while(t--){     solve(); }
+    return 0;
+}
---
+++ Topic_Wise/Two Pointers/Practice/tempCodeRunnerFile.cpp
@@ -0,0 +1,25 @@
+while(l < r ){
+        
+    //     bool ist = true;
+    //     if(v[r].ff + v[l].ff < x){
+
+    //         int k = x - (v[r].ff + v[l].ff);
+    //         // for(int i = 0; i < n; i++){
+
+    //         //     if(v[i].ff > k) break;
+    //         //     if(v[i].ff < k) ist = false;
+
+    //         //     if(v[i].ss != v[l].ss && v[r].ss != v[i].ss){
+
+    //         //         if(v[i].ff == k){
+    //         //             cout << v[i].ss + 1 << " " << v[l].ss + 1 << " " << v[r].ss + 1 << nline;
+    //         //             return;
+    //         //         }
+    //         //     }
+    //         // }
+
+    //     }
+
+    //     else if(v[r].ff + v[l].ff >= x || ist) r--;
+    //     else            l++;
+    // }
