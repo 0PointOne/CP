2eec0a90917aa8f323044366b2661620f978a6c2
0PointOne <zishanabder@gmail.com>
2024-04-05 20:41:15 +0600

    two pointers practice

---
+++ Topic_Wise/Two Pointers/Practice/C_Alternating_Subsequence.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define int long long
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define print_v(v) for(auto it : v) cout << it << " "; cout << nline;
+#define print_v_pair(v) for(auto it: v) cout << it.first << " " << it.second << nline;
+#define input_v for(auto &it : v)   cin >> it;
+
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << nline;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve(){
+
+    int n;  cin >> n;
+    vector<int> v(n);
+    input_v(v);
+
+    int sum = 0;
+    int s = 0, f = (v[0] < 0  ?  -1  : 1), tr = INT_MIN;
+    while(s < n){
+        
+        if((f == -1 && v[s] < 0) || (f == 1 && v[s] > 0)){
+            tr = max(tr, v[s]);
+        }  
+        else if(f == -1 && v[s] > 0){       
+            sum += tr; 
+            f = 1; 
+            tr = v[s];
+        }
+        else if(f == 1 && v[s] < 0){       
+            sum += tr; 
+            f = -1; 
+            tr = v[s];
+        }
+        s++;
+
+    }
+    sum += tr;
+    
+    cout << sum << nline;
+    
+}
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    fastio();
+    int t = 1;   	
+    cin >> t;
+    while(t--){     solve(); }
+    return 0;
+}
