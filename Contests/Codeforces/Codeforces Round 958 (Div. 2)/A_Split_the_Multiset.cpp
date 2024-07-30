7bf30b31e76e596940fa3fbbb57a061583ac033d
0PointOne <zishanabder@gmail.com>
2024-07-30 23:04:21 +0600

    958 Div 2 A

---
+++ Contests/Codeforces/Codeforces Round 958 (Div. 2)/A_Split_the_Multiset.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+void solve(){
+
+    int n, k;   cin >> n >> k;
+    
+    // int cnt = 0;
+    // while(n > 1){
+    //     cnt++;
+    //     n -= (k - 1);
+    // }
+
+    // cout << cnt << "\n";
+    cout << (n + k - 3) / (k -1) << "\n";
+
+}
+
+signed main(){
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--){solve();}
+    return 0;
+}
