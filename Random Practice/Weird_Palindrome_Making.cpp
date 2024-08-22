ac30042782f901f67e5ebce18572030e8c9d6691
0PointOne <zishanabder@gmail.com>
2024-08-22 14:36:21 +0600

    Practice

---
+++ Random Practice/Weird_Palindrome_Making.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+void solve(){
+
+    int n;  cin >> n;
+    int odd = 0;
+    for(int i = 0; i < n; i++){
+        int x;  cin >> x;
+        if(x & 1) odd++;
+    }
+    cout << (odd > 0 ? (odd/2) : 0) << "\n";
+}
+
+signed main(){
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--){solve();}
+    return 0;
+}
