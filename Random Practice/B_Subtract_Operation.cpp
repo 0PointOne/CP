98dbb178cfd8176881648ff093c8abfd243787bd
0PointOne <zishanabder@gmail.com>
2024-08-27 04:37:57 +0600

    practice

--- Random Practice/B_Subtract_Operation.cpp
+++ Random Practice/B_Subtract_Operation.cpp
@@ -1,67 +1,7 @@
 #include<bits/stdc++.h>
+#define int long long int
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
-
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define int long long
-#define PI 3.141592653589793238462
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-typedef unsigned long long ull;
-typedef long double lld;
-
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << nline;
-#else
-#define debug(x)
-#endif
-
-void _print(int t) {cerr << t;}
-void _print(bool t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-
-
-bool isFound(int s, vector<int> &v, int key){
-    int e = sz(v) - 1;
-
-    int mid = s + (e - s) / 2;
-    while(s <= e){
-        if(v[mid] == key){
-            return true;
-        }
-        else if(v[mid] > key) e = mid - 1;
-        else             s = mid + 1;
-
-        mid = s + (e - s) / 2;
-    }
-
-    return false;
-}
 
 void solve(){
 
@@ -69,29 +9,34 @@ void solve(){
     vector<int> v(n);
     for(int i = 0; i < n; i++)  cin >> v[i];
 
-    sort(all(v));
+    sort(v.begin(), v.end());
+    int i = 0, j = i+1;
+    int sum = v[j] - v[i];
 
-    for(int i = 0; i < n; i++){
-        int key = v[i] + k;
+    while(i < n && j < n){
 
-        if(isFound(i, v, key)){
-            cout << "YES" << nline;
+        if(sum > k){
+            i++;
+            if(i < n) sum = v[j] - v[i];
+        }
+        else if(sum < k){
+            j++;
+            if(j < n) sum = v[j] - v[i];
+        }
+
+        if(sum == k){
+            cout << "YES" << "\n";
             return;
         }
     }
+    cout << "NO" << "\n";
 
-    cout << "NO" << nline;
-    
 }
 
-signed main() {
-#ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr);
-#endif
-
-    fastio();
-    int t = 1;   	
+signed main(){
+    fast;
+    int t = 1;
     cin >> t;
-    while(t--){     solve(); }
+    while(t--){solve();}
     return 0;
 }
--- Random Practice/F_Eating_Candies.cpp
+++ Random Practice/F_Eating_Candies.cpp
@@ -1,50 +1,42 @@
 #include<bits/stdc++.h>
+#define int long long int
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define int long long
-#define PI 3.141592653589793238462
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
 
-typedef unsigned long long ull;
-typedef long double lld;
+void binarySearch__(int n, vector<int>& v){
+    vector<int> pr(n+1);
+    for(int i = 1; i <= n; i++){
+        pr[i] = pr[i-1] + v[i-1];
+    }
 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << nline;
-#else
-#define debug(x)
-#endif
+    int mx = 0;
+    for(int i = n; i >= 1; i--){
+        int idx = lower_bound(pr.begin(), pr.end(), pr.back() - pr[i-1]) - pr.begin();
+        if(idx <= n && idx < i && pr[idx] == pr.back() - pr[i-1]){
+            mx = max(n - i + 1 + idx, mx);
+        }
+    }
+    cout << mx << "\n";
+}
 
-void _print(int t) {cerr << t;}
-void _print(bool t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
+void twoPointer(int n, vector<int>& v){
+    int ans = 0;
+    int l = 0, r = n-1;
+    int lSum = 0, rSum = 0;
+    while(l <= r){
 
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+        if(lSum < rSum)      lSum += v[l++];
+        else if(lSum > rSum) rSum += v[r--];
+        
+        if(lSum == rSum){
+            ans = l + n - r - 1;
+            if(l <= r) lSum += v[l++];
+        }
+    }
 
+    cout << ans << "\n";
+}
 
 void solve(){
 
@@ -52,54 +44,15 @@ void solve(){
     vector<int> v(n);
     for(int i = 0; i < n; i++)  cin >> v[i];
 
-    if(n == 1){
-        cout << 0 << nline;
-        return;
-    }
+    // twoPointer(n, v);
+    binarySearch__(n, v);
 
-    int s = 1, e = n - 2;
-
-    int ans = 0;
-    int l_sum = v[0], r_sum = v.back();
-    if(l_sum == r_sum){
-        ans = 2;
-    }
-    
-    while(s <= e){
-        
-        if(l_sum == r_sum){
-            ans = s + (n - e - 1);
-
-            l_sum += v[s++];
-        }
-        while(s <= e && l_sum < r_sum){
-            l_sum += v[s++];
-        }
-
-        while(s <= e && r_sum < l_sum){
-            r_sum += v[e--];
-        }
-
-        if(l_sum == r_sum){
-            ans = s + (n - e - 1);
-
-            l_sum += v[s++];
-        }
-
-    }
-
-    cout << ans << nline;
-    
 }
 
-signed main() {
-#ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr);
-#endif
-
-    fastio();
-    int t = 1;   	
+signed main(){
+    fast;
+    int t = 1;
     cin >> t;
-    while(t--){     solve(); }
+    while(t--){solve();}
     return 0;
 }
