29b367ffa9530c676090ae5e85b7ec88eac22663
0PointOne <zishanabder@gmail.com>
2024-09-20 09:50:55 +0600

    segemnt tree practice

---
+++ Topic_Wise/Segment_Tree/Practice/Static_Range_Minimum_Queries.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+const int N = 2e5 + 5;
+int v[N], tr[3*N];
+
+void build(int n, int l, int r){
+    if(l == r){
+        tr[n] = v[l];
+        return;
+    }
+    int mid = l + (r - l) / 2;
+
+    build(2*n, l, mid);
+    build(2*n+1, mid+1, r);
+
+    tr[n] = min(tr[2*n], tr[2*n+1]);
+}
+
+
+int mn(int n, int l, int r, int a, int b){
+    if(l > b || r < a) return INT_MAX;
+    if(l >= a && r <= b) return tr[n];
+
+    int mid = l + (r - l) / 2;
+    return min(mn(2*n, l, mid, a, b), mn(2*n+1, mid+1, r, a, b));
+}
+
+
+
+void solve(){
+
+    int n, q;   cin >> n >> q;
+    for(int i = 1; i <= n; i++) cin >> v[i];
+
+    build(1, 1, n);
+
+    while(q--){
+        int l, r;   cin >> l >> r;
+        cout << mn(1, 1, n, l, r) << "\n";
+    }
+}
+
+signed main(){
+    fast;
+    int t = 1;
+    //cin >> t;
+    while(t--){solve();}
+    return 0;
+}
